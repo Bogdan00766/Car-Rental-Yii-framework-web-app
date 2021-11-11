b65c29d70da8ed4a9861a71800812f99719604cf
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-11 20:11:04 +0000

    add navbar

--- Stage2/index.php
+++ Stage2/index.php
@@ -16,7 +16,28 @@
             </div>
 
             <div class="col-md-8">
-                NAVBAR
+
+                <nav class="navbar navbar-expand-lg">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav navbar-back">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Vehicles</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Discover</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Feedback</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
 
             <div class="col-md-2 login-button">
