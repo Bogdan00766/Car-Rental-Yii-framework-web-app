3289c925816c41213656fdd34f08959491608532
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-11 19:20:45 +0000

    add index.php

---
+++ Stage2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Car Rental Website</title>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+</head>
+<body>
+
+    <script src="vendor/components/jquery/jquery.js"></script>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
+</body>
+</html>
