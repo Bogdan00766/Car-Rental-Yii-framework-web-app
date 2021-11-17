4297f9e4bda8e89cce9adba17ddb89b8274d3191
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-17 23:24:26 +0000

    Define login-col and register-col

--- Stage2/pages/login.php
+++ Stage2/pages/login.php
@@ -1,9 +1,9 @@
 <div class="container">
     <div class="row">
-        <div class="col">
+        <div class="col login-col">
 
         </div>
-        <div class="col">
+        <div class="col register-col">
 
         </div>
     </div>
