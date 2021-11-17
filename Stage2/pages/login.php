4d230fe151e6a2340320d51c6591ec0b154595a6
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-17 23:26:18 +0000

    Add loginInputEmail form

--- Stage2/pages/login.php
+++ Stage2/pages/login.php
@@ -1,7 +1,9 @@
 <div class="container">
     <div class="row">
         <div class="col login-col">
-
+            <form>
+                <label for="loginInputEmail">Email</label>
+            </form>
         </div>
         <div class="col register-col">
 
