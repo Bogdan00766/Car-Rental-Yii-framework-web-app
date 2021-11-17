7b88ffd5da47e20ad176e87156a50dfeb95769cb
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-17 23:31:39 +0000

    add loginInputPassword form group

--- Stage2/pages/login.php
+++ Stage2/pages/login.php
@@ -6,6 +6,11 @@
                     <label for="loginInputEmail">Email address</label>
                     <input type="email" class="form-control" id="loginInputEmail">
                 </div>
+
+                <div class="form-group">
+                    <label for="loginInputPassword">Password</label>
+                    <input type="password" class="form-control" id="loginInputPassword">
+                </div>
             </form>
         </div>
         <div class="col register-col">
