b160e1e85ae2c168b397172e6ddbb906497a3ac2
Marija <m308243@alunos.ipb.pt>
2021-11-25 19:19:50 +0100

    add top margins

--- Stage2/pages/features.php
+++ Stage2/pages/features.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <div class="row">
+    <div class="row features-row">
         <div class ="col-4"> </div>
         <div class="col-4 features-text">
             We offer a complete <br>range of features
