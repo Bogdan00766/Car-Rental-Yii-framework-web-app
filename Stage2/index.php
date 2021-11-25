6f2678870f299524b58deafd6c5901be9653e92e
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-25 17:39:25 +0000

    Linked new stylesheets to main project

--- Stage2/index.php
+++ Stage2/index.php
@@ -3,6 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Car Rental Website</title>
+    <?php
+    $file = "pages/home.php";
+    if(isset($_GET["page"])) {
+        $page = $_GET['page'];
+        $f = "pages/$page.php";
+        if(file_exists($f)){
+            $file = $f;
+        }
+    }
+    ?>
+
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/home_style.css">
@@ -22,16 +33,7 @@
                 <nav class="navbar navbar-expand-lg navbar-css">
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <?php
-                            $file = "pages/home.php";
-                            if(isset($_GET["page"])) {
-                                $page = $_GET['page'];
-                                $f = "pages/$page.php";
-                                if(file_exists($f)){
-                                    $file = $f;
-                                }
-                            }
-                            ?>
+
                             <a class="nav-link navbar-element <?php if($page=='home' || is_null($page)) echo("active")?>" href="index.php?page=home">Home</a>
                             <a class="nav-link navbar-element <?php if($page=='features') echo("active")?>" href="index.php?page=features">Features</a>
                             <a class="nav-link navbar-element <?php if($page=='vehicles') echo("active")?>" href="index.php?page=vehicles">Vehicles</a>
