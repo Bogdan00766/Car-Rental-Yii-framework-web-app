5e8262a02047bd6164832fa96ef0f639de935abc
Marija <m308243@alunos.ipb.pt>
2021-11-25 19:56:29 +0100

    added price tag item

--- Stage2/pages/features.php
+++ Stage2/pages/features.php
@@ -18,7 +18,7 @@
 
         </div>
         <div class ="col-4 icon-column">
-            <i class="fas fa-shopping-cart fa-3x icon-theme"></i>
+            <i class="fas fa-tag fa-3x icon-theme"></i>
         </div>
 
     </div>
