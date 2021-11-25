fbeb727f6e31d2a6e12474c7149fdec9f2f6ebfa
Marija <m308243@alunos.ipb.pt>
2021-11-25 20:09:34 +0100

    put text for flexible rentals

--- Stage2/pages/features.php
+++ Stage2/pages/features.php
@@ -15,6 +15,9 @@
             <p class= "title">
                 Flexible rentals
             </p>
+            <p>
+            Cancel or change most bookings for free up to 48 hours before pick-up
+            </p>
         </div>
         <div class="col-4 icon-column">
             <i class="fas fa-shield-alt fa-3x icon-theme"></i>
