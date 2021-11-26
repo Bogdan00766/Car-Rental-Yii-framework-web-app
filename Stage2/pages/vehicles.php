db3f94227f6aaa324b7ff334b980b1c30fb590af
Marija <m308243@alunos.ipb.pt>
2021-11-26 21:50:49 +0100

    put the text under pictures

--- Stage2/pages/vehicles.php
+++ Stage2/pages/vehicles.php
@@ -9,47 +9,29 @@
 
 
     <div class="row">
-        <div class ="col-fluid-3"> </div>
-        <img class="image" src="photos/red-car-pic.webp">
-
-        <div class="col-fluid-3"> </div>
-        <img class="image" src="photos/blu car more small.webp">
-
-        <div class ="col-fluid-3"> </div>
-        <img class="image" src="photos/gray more small.webp">
-
-        <div class ="col-fluid-3"> </div>
-        <img class="image" src="photos/black small audi more.webp">
-
-    </div>
-
-    <div class="container-fluid">
-        <div class="row features-row">
-            <div class ="col-fluid-3">
+        <div class ="col-fluid-3">
+            <img class="image" src="photos/red-car-pic.webp">
             <p class="vehicles-text2">
                 Mini
             </p>
-            </div>
 
-            <div class ="col-fluid-3">
+        </div>
+        <div class="col-fluid-3">
+            <img class="image" src="photos/blu car more small.webp">
             <p class="vehicles-text2">
                 Economy
             </p>
-            </div>
-
-            <div class ="col-fluid-3">
+        </div>
+        <div class ="col-fluid-3">
+            <img class="image" src="photos/gray more small.webp">
             <p class="vehicles-text2">
                 Compact
             </p>
-            </div>
-
-            <div class ="col-fluid-3">
+        </div>
+        <div class ="col-fluid-3">
+            <img class="image" src="photos/black small audi more.webp">
             <p class="vehicles-text2">
                 Full size
             </p>
-            </div>
         </div>
-        </div>
-
-
-
+    </div>
