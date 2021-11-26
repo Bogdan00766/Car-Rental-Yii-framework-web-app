1c9c8f0703157f973829a415a3eb21fb65fcda09
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-26 20:49:19 +0000

    Added button

--- Stage2/pages/home.php
+++ Stage2/pages/home.php
@@ -8,6 +8,8 @@
                 We are your Rent a Car that offers you the best vehicles, at the best prices, so that you have a peaceful stay.
                 We value the well-being and satisfaction of our customers and, therefore, from your arrival until your departure, we want you to feel like at home.
             </p>
+            <a class="btn btn-primary" href="index.php?page=vehicles">Find your car</a>
+
         </div>
 
         <div class="col-7">
