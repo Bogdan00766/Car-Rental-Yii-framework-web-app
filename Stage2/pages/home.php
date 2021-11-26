20c88daa0d287126c777c814845baae87b7f5515
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-26 20:13:20 +0000

    Created row with 2 columns

--- Stage2/pages/home.php
+++ Stage2/pages/home.php
@@ -1,1 +1,14 @@
-Home
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            TEsT
+
+        </div>
+
+        <div class="col-4>
+
+
+        </div>
+    </div>
+
+</div>
