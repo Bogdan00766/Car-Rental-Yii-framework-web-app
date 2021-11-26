3752ef96304957c97e808bee7c556de22feef5ea
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-26 20:39:18 +0000

    Added text

--- Stage2/pages/home.php
+++ Stage2/pages/home.php
@@ -1,8 +1,13 @@
 <div class="container">
     <div class="row home-main-row">
         <div class="col-6">
-            <p class="home-title">We help you find the best solution</p>
-
+            <p class="home-title">
+                We help you find the best solution
+            </p>
+            <p>
+                We are your Rent a Car that offers you the best vehicles, at the best prices, so that you have a peaceful stay.
+                We value the well-being and satisfaction of our customers and, therefore, from your arrival until your departure, we want you to feel like at home.
+            </p>
         </div>
 
         <div class="col-4">
