a236fdef8db77920a5c6f5563380b7a33163474b
Marija <m308243@alunos.ipb.pt>
2021-11-26 21:17:08 +0100

    put title on vehicles page

--- Stage2/pages/vehicles.php
+++ Stage2/pages/vehicles.php
@@ -1,1 +1,10 @@
-vehicles
+<div class="container">
+    <div class="row features-row">
+        <div class ="col-12">
+        <p class="vehicles-text">
+            CAR RENTAL IN OVER 100 CONTRIES AND TERRITORIES
+        </p>
+        </div>
+    </div>
+
+
